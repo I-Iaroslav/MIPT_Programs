77eddf7660c902f98b512931c8d6524b4b6fecc0
I-Iaroslav <40538879+I-Iaroslav@users.noreply.github.com>
2021-10-20 17:03:05 +0300

    Add files via upload

---
+++ FS/ps.c
@@ -0,0 +1,36 @@
+#include <dirent.h> 
+#include <stdio.h> 
+
+
+int main(void) {
+    DIR *d;
+    FILE *f;
+    struct dirent *dir;
+
+    
+    char proc_dir[] = "/proc";
+
+    d = opendir("/proc");
+
+    printf("PID     CMD\n");
+
+    if (d) {
+        while ((dir = readdir(d)) != NULL) {
+            if(dir->d_name[0] >= '0' && dir->d_name[0] <= '9') {
+
+                char stat_way[11 + sizeof(dir->d_name)];
+                snprintf(stat_way, sizeof(stat_way), "%s/%s/stat", proc_dir, dir->d_name);
+
+                f = fopen(stat_way, "r");
+
+                int pid;
+                char proc_name[255];
+                fscanf(f, "%d %s", &pid, proc_name);
+
+                printf("%-8d%s\n", pid, proc_name);
+            }
+        }    
+        closedir(d);
+    }
+    return(0);
+}
