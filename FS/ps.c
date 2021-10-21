c2c81b6adf9359262984c73396666b7c5722c102
I-Iaroslav <40538879+I-Iaroslav@users.noreply.github.com>
2021-10-21 21:24:06 +0300

    Update ps.c

--- FS/ps.c
+++ FS/ps.c
@@ -1,6 +1,14 @@
 #include <dirent.h> 
 #include <stdio.h> 
+#include <errno.h> 
+#include <stdlib.h> 
 
+void check(char* message) {
+    if(errno != 0) {
+        printf("Error! %s fail!\n", message);
+        exit(1);
+    }
+}
 
 int main(void) {
     DIR *d;
@@ -9,23 +17,28 @@ int main(void) {
 
     
     char proc_dir[] = "/proc";
-
-    d = opendir("/proc");
+    
+    d = opendir(proc_dir);
+    check("opendir");
 
     printf("PID     CMD\n");
 
     if (d) {
         while ((dir = readdir(d)) != NULL) {
+            check("readdir");
+
             if(dir->d_name[0] >= '0' && dir->d_name[0] <= '9') {
 
                 char stat_way[11 + sizeof(dir->d_name)];
                 snprintf(stat_way, sizeof(stat_way), "%s/%s/stat", proc_dir, dir->d_name);
 
                 f = fopen(stat_way, "r");
+                check("fopen");
 
                 int pid;
                 char proc_name[255];
                 fscanf(f, "%d %s", &pid, proc_name);
+                check("fscanf");
 
                 printf("%-8d%s\n", pid, proc_name);
 
@@ -34,5 +47,5 @@ int main(void) {
         }    
         closedir(d);
     }
-    return(0);
+    return 0;
 }
